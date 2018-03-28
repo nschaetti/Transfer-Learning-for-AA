db436a68b14eb345f10e9c75e3616b2703fc6a1e
nschaetti <n.schaetti@gmail.com>
2018-03-28 14:33:04 +0200

    Transfer learning

---
+++ transfer_learning.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : core.classifiers.RCNLPTextClassifier.py
+# Description : Echo State Network for text classification.
+# Auteur : Nils Schaetti <nils.schaetti@unine.ch>
+# Date : 01.02.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the Reservoir Computing NLP Project.
+# The Reservoir Computing Memory Project is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports
+import argparse
+import torch.utils.data
+from modules import CNNClassifier
+from echotorch import datasets
+from echotorch.transforms import text
+from torch.autograd import Variable
+import torch.nn as nn
+from torch import optim
+import os
+from authors import authors as aa
+
+# Settings
+n_epoch = 600
+embedding_dim = 300
+n_authors = 15
+use_cuda = False
+
+# Argument parser
+parser = argparse.ArgumentParser(description="CNN transfer learning")
+
+# Argument
+parser.add_argument("--input", type=str, help="Pre-trained classifier file", default='.')
+parser.add_argument("--n-authors", type=int, help="Number of authors to pre-train", default=20)
+args = parser.parse_args()
+
+# Authors
+authors = aa[:args.n_authors]
+
+# Word embedding
+transform = text.Character()
+
+# Reuters C50 dataset
+reutersloader = torch.utils.data.DataLoader(
+    datasets.ReutersC50Dataset(download=True, authors=authors, transform=transform),
+    batch_size=1, shuffle=False)
+
+
