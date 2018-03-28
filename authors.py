671eb68e62aec4901914582a5bd6d237016ba12e
nschaetti <n.schaetti@gmail.com>
2018-03-28 14:32:04 +0200

    Authors

---
+++ authors.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Authors
+authors = ["AlexanderSmith", "WilliamKazer", "MarcelMichelson", "GrahamEarnshaw", "JanLopatka", "MichaelConnor",
+           "MartinWolk", "ToddNissen", "PatriciaCommins", "KevinMorrison", "HeatherScoffield", "BradDorfman",
+           "DavidLawder", "JohnMastrini", "LynnleyBrowning", "ScottHillis", "FumikoFujisaki", "TimFarrand",
+           "SarahDavison", "AaronPressman", "KevinDrawbaugh", "NickLouth", "PierreTran", "RogerFillion",
+           "MatthewBunce", "KouroshKarimkhany", "JimGilchrist", "MureDickie", "DarrenSchuettler", "TanEeLyn",
+           "JoeOrtiz", "LynneO'Donnell", "EdnaFernandes", "JoWinterbottom", "BenjaminKangLim", "KirstinRidley",
+           "JonathanBirt", "BernardHickey", "RobinSidel", "AlanCrosby", "LydiaZajc", "PeterHumphrey",
+           "KeithWeir", "EricAuchard", "TheresePoletti", "KarlPenhaul", "SimonCowell", "JaneMacartney",
+           "SamuelPerry", "MarkBendeich"]
