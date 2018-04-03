4800f38861c71279be34f798f5d211cdfcbe1415
nschaetti <n.schaetti@gmail.com>
2018-04-03 18:16:46 +0200

    CNN classifier

--- modules/CNNClassifier.py
+++ modules/CNNClassifier.py
@@ -5,6 +5,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 
 # CNN classifier
@@ -14,7 +15,7 @@ class CNNClassifier(nn.Module):
     """
 
     # Constructor
-    def __init__(self, voc_size, embedding_dim, n_authors, out_channels=(20, 10), kernel_sizes=(5, 5), max_pool_size=2, n_features=30):
+    def __init__(self, voc_size, embedding_dim, n_authors, window_size, out_channels=(100, 100, 100), kernel_sizes=(3, 4, 5), temporal_max=300, n_features=30):
         """
         Constructor
         :param n_authors:
@@ -28,17 +29,31 @@ class CNNClassifier(nn.Module):
         # Embedding layer
         self.embedding = nn.Embedding(voc_size, embedding_dim)
 
-        # Conv 1
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=out_channels[0], kernel_size=kernel_sizes[0])
+        # Conv window 1
+        self.conv_w1 = nn.Conv2d(in_channels=1, out_channels=out_channels[0],
+                                 kernel_size=(kernel_sizes[0], embedding_dim))
+
+        # Conv window 2
+        self.conv_w2 = nn.Conv2d(in_channels=1, out_channels=out_channels[1],
+                                 kernel_size=(kernel_sizes[1], embedding_dim))
+
+        # Conv window 3
+        self.conv_w3 = nn.Conv2d(in_channels=1, out_channels=out_channels[2],
+                                 kernel_size=(kernel_sizes[2], embedding_dim))
 
         # Max pooling layer
-        self.max_pool = nn.MaxPool1d(kernel_size=max_pool_size, stride=0)
-
-        # Conv 2
-        self.conv2 = nn.Conv1d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=kernel_sizes[1])
+        """self.max_pool_w1 = nn.MaxPool1d(kernel_size=window_size - out_channels[0] + 1, stride=0)
+        self.max_pool_w2 = nn.MaxPool1d(kernel_size=window_size - out_channels[1] + 1, stride=0)
+        self.max_pool_w3 = nn.MaxPool1d(kernel_size=window_size - out_channels[2] + 1, stride=0)"""
+        self.max_pool_w1 = nn.MaxPool1d(kernel_size=temporal_max, stride=temporal_max)
+        self.max_pool_w2 = nn.MaxPool1d(kernel_size=temporal_max, stride=temporal_max)
+        self.max_pool_w3 = nn.MaxPool1d(kernel_size=temporal_max, stride=temporal_max)
 
         # Linear layer 1
-        self.linear_size = out_channels[1] * 72
+        # self.linear_size = out_channels[1] * 72
+        self.temporal_features = int(math.floor(window_size / temporal_max))
+        self.linear_size = self.temporal_features * out_channels[0] + self.temporal_features * out_channels[
+            1] + self.temporal_features * out_channels[2]
         self.linear = nn.Linear(self.linear_size, n_features)
 
         # Linear layer 2
@@ -53,22 +68,31 @@ class CNNClassifier(nn.Module):
         :return:
         """
         # Embedding layer
-        embed = self.embedding(x)
+        embeds = self.embedding(x)
 
-        # Conv 1
-        out_conv1 = F.relu(self.conv1(embed))
+        # Add channel dim
+        embeds = torch.unsqueeze(embeds, dim=1)
+
+        # Conv window
+        out_win1 = F.relu(self.conv_w1(embeds))
+        out_win2 = F.relu(self.conv_w2(embeds))
+        out_win3 = F.relu(self.conv_w3(embeds))
+
+        # Remove last dim
+        out_win1 = torch.squeeze(out_win1, dim=3)
+        out_win2 = torch.squeeze(out_win2, dim=3)
+        out_win3 = torch.squeeze(out_win3, dim=3)
 
         # Max pooling
-        max_pooled = self.max_pool(out_conv1)
+        max_win1 = self.max_pool_w1(out_win1)
+        max_win2 = self.max_pool_w2(out_win2)
+        max_win3 = self.max_pool_w3(out_win3)
 
-        # Conv 2
-        out_conv2 = F.relu(self.conv2(max_pooled))
-
-        # Max pooling
-        max_pooled = self.max_pool(out_conv2)
+        # Concatenate
+        out = torch.cat((max_win1, max_win2, max_win3), dim=1)
 
         # Flatten
-        out = max_pooled.view(-1, self.linear_size)
+        out = out.view(-1, self.linear_size)
 
         # Linear 1
         out = F.relu(self.linear(out))
