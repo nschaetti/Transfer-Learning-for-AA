6ef775116aa97d7133a4bc108f07fee55fe45f92
nschaetti <n.schaetti@gmail.com>
2018-04-03 22:15:42 +0200

    Transfer learning

--- transfer_learning.py
+++ transfer_learning.py
@@ -25,14 +25,13 @@
 # Imports
 import argparse
 import torch.utils.data
-from modules import CNNClassifier
 from echotorch import datasets
 from echotorch.transforms import text
 from torch.autograd import Variable
 import torch.nn as nn
 from torch import optim
-import os
 from authors import authors as aa
+import os
 
 # Settings
 n_epoch = 600
@@ -45,18 +44,129 @@ parser = argparse.ArgumentParser(description="CNN transfer learning")
 
 # Argument
 parser.add_argument("--input", type=str, help="Pre-trained classifier file", default='.')
+parser.add_argument("--character-gram", type=int, help="Character gram", default=2)
 parser.add_argument("--n-authors", type=int, help="Number of authors to pre-train", default=20)
+parser.add_argument("--batch-size", type=int, help="Batch size", default=64)
+parser.add_argument("--epoch", type=int, help="Epoch", default=500)
+parser.add_argument("--pretraining", type=str, help="Pre-training type (finetuning, extractor)", default="finetuning")
 args = parser.parse_args()
 
 # Authors
 authors = aa[:args.n_authors]
 
+# Load pre-trained model
+model, gram_to_ix = torch.load(open(args.input, 'rb'))
+
+# If feature extract, then block params
+if args.pretraining == "extractor":
+    for param in model.parameters():
+        param.requires_grad = False
+    # end for
+# end if
+
 # Word embedding
-transform = text.Character()
+if args.character_gram == 1:
+    transform = text.Character(fixed_length=7978, gram_to_ix=gram_to_ix)
+    window_size = 7978
+    voc_size = 60
+elif args.character_gram == 2:
+    transform = text.Character2Gram(fixed_length=7978, gram_to_ix=gram_to_ix)
+    window_size = 7978
+    voc_size = 1653
+elif args.character_gram == 3:
+    transform = text.Character3Gram(fixed_length=7977, gram_to_ix=gram_to_ix)
+    window_size = 7977
+    voc_size = 16889
+# end if
 
 # Reuters C50 dataset
 reutersloader = torch.utils.data.DataLoader(
     datasets.ReutersC50Dataset(download=True, authors=authors, transform=transform),
-    batch_size=1, shuffle=False)
+    batch_size=args.batch_size, shuffle=False)
 
+# Model
+model.linear2 = nn.Linear(model.linear_size, args.n_authors)
+if use_cuda:
+    model.cuda()
+# end if
 
+# Loss function
+loss_function = nn.CrossEntropyLoss()
+
+# Optimizer
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# First fold and training mode
+reutersloader.dataset.set_fold(0)
+
+# Epoch
+for epoch in range(args.epoch):
+    # Total losses
+    training_loss = 0.0
+    test_loss = 0.0
+
+    # Get test data for this fold
+    for i, data in enumerate(reutersloader):
+        # Inputs and labels
+        inputs, labels, time_labels = data
+
+        # To variable
+        inputs, outputs = Variable(inputs), Variable(torch.LongTensor(labels))
+        if use_cuda:
+            inputs, outputs = inputs.cuda(), outputs.cuda()
+        # end if
+
+        # Zero grad
+        model.zero_grad()
+
+        # Compute output
+        log_probs = model(inputs)
+
+        # Loss
+        loss = loss_function(log_probs, outputs)
+
+        # Backward and step
+        loss.backward()
+        optimizer.step()
+
+        # Add
+        training_loss += loss.data[0]
+    # end for
+
+    # Set test mode
+    reutersloader.dataset.set_train(False)
+
+    # Counters
+    total = 0.0
+    success = 0.0
+
+    # For each test sample
+    for i, data in enumerate(reutersloader):
+        # Inputs and labels
+        inputs, labels, time_labels = data
+
+        # To variable
+        inputs, outputs = Variable(inputs), Variable(torch.LongTensor(labels))
+        if use_cuda:
+            inputs, outputs = inputs.cuda(), outputs.cuda()
+        # end if
+
+        # Forward
+        model_outputs = model(inputs)
+        loss = loss_function(model_outputs, outputs)
+
+        # Take the max as predicted
+        _, predicted = torch.max(model_outputs.data, 1)
+
+        # Add to correctly classified word
+        success += (predicted == outputs.data).sum()
+        total += predicted.size(0)
+
+        # Add loss
+        test_loss += loss.data[0]
+    # end for
+
+    # Print and save loss
+    print(u"Epoch {}, training loss {}, test loss {}, accuracy {}".format(epoch, training_loss, test_loss,
+                                                                          success / total * 100.0))
+# end for
