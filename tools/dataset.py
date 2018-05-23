713a3942046999d08c7a7ea885d7605ca4e2350d
nschaetti <n.schaetti@gmail.com>
2018-05-23 18:59:55 +0200

    Dataset

--- tools/dataset.py
+++ tools/dataset.py
@@ -3,6 +3,7 @@
 #
 
 # Imports
+import datasets
 import torchlanguage.transforms
 import torch
 
@@ -12,14 +13,15 @@ import torch
 #########################################
 
 
-# Load dataset
-def load_dataset(authors=None):
+# Load authorship attribution dataset
+def load_AA_dataset(authors=None):
     """
     Load dataset
     :return:
     """
     # Load from directory
     reutersc50_dataset = torchlanguage.datasets.ReutersC50Dataset(
+        root='./aa_data',
         authors=authors,
         download=True
     )
@@ -39,3 +41,31 @@ def load_dataset(authors=None):
     )
     return reutersc50_dataset, reuters_loader_train, reuters_loader_test
 # end load_dataset
+
+
+# Load author profiling dataset
+def load_AP_dataset(text_transform, batch_size):
+    """
+    Load author profiling dataset
+    :return:
+    """
+    # Tweet data set 2017 training
+    tweet_dataset_17 = datasets.TweetDataset(root='./data/', download=True, lang='en',
+                                            text_transform=text_transform)
+
+    # Loader
+    pan17loader_training = torch.utils.data.DataLoader(
+        torchlanguage.utils.CrossValidation(tweet_dataset_17, train=True),
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    # Loader
+    pan17loader_validation = torch.utils.data.DataLoader(
+        torchlanguage.utils.CrossValidation(tweet_dataset_17, train=False),
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    return pan17loader_training, pan17loader_validation
+# end load_AP_dataset
